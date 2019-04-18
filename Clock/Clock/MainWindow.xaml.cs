a5cb5c8294ca70ae0dbeb2e0807b2ebd2c6cbd10
Patrick O' Riordan <keshmedia@users.noreply.github.com>
2019-04-18 18:23:18 +0100

    Add files via upload

---
+++ Clock/Clock/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace Clock
+{
+ public partial class MainWindow : Window
+    {
+   public MainWindow()
+        {
+ InitializeComponent();
+
+ Storyboard seconds = (Storyboard)second.FindResource("sbseconds");
+ seconds.Begin();
+ seconds.Seek(new TimeSpan(0, 0, 0, 
+ DateTime.Now.Second, 0));
+
+ Storyboard minutes = (Storyboard)minute.FindResource("sbminutes");
+ minutes.Begin();
+ minutes.Seek(new TimeSpan(0, 0, DateTime.Now.Minute, 
+DateTime.Now.Second, 0));
+
+ Storyboard hours = (Storyboard)hour.FindResource("sbhours");
+ hours.Begin();
+ hours.Seek(new TimeSpan(0, DateTime.Now.Hour, 
+DateTime.Now.Minute, DateTime.Now.Second, 0));
+ }
+ private void Window_MouseDown(object sender, 
+MouseButtonEventArgs e)
+ {
+ {
+  try
+  {
+      DragMove();
+  }
+  catch
+  {
+      MessageBox.Show("Paddy O'Riordan");
+  } }}  }}  // THE C# CODE /////
